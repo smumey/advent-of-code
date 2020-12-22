1048b1b78df0d830c78fcffbc483177118fa1216
Sol Mumey <smumey@gmail.com>
2020-12-22 01:25:57 -0700

    aoc 13b

--- 13/13-bus.go
+++ 13/13-bus.go
@@ -8,36 +8,59 @@ import (
 	"strings"
 )
 
-func convertBuses(raw []string) []int {
-	var buses []int
-	for _, r := range raw {
-		b, err := strconv.Atoi(r)
-		if err == nil {
-			buses = append(buses, b)
-		}
-	}
-	return buses
+typeq busT struct {
+	number int
+	delay  int
 }
 
-func findBusWait(arrival int, buses []int) int {
+func findBusWait(arrival int, buses []busT) int {
 	bestWait := -1
 	bestBus := 0
 	for _, b := range buses {
-		w := b - (arrival % b)
+		w := b.number - (arrival % b.number)
 		if w < bestWait || bestWait == -1 {
 			bestWait = w
-			bestBus = b
+			bestBus = b.number
 		}
 	}
 	return bestBus * bestWait
 }
 
+func findConvergentTime(buses []busT) int {
+	fmt.Println(buses)
+	t := 1
+	inc := 1
+	for _, b := range buses {
+		for s := t; ; s += inc {
+			if (s+b.delay)%b.number == 0 {
+				t = s
+				inc *= b.number
+				fmt.Printf("bus %v t %d\n", b, t)
+				break
+			}
+		}
+	}
+	return t
+}
+
+func toBuses(raw []string) []busT {
+	var buses []busT
+	for i, r := range raw {
+		b, err := strconv.Atoi(r)
+		if err == nil {
+			buses = append(buses, busT{b, i})
+		}
+
+	}
+	return buses
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	arrival, _ := strconv.Atoi(scanner.Text())
+	// arrival, _ := strconv.Atoi(scanner.Text())
 	scanner.Scan()
-	buses := convertBuses(strings.Split(scanner.Text(), ","))
-	fmt.Printf("time: %d buses: %v\n", arrival, buses)
-	fmt.Println(findBusWait(arrival, buses))
+	buses := toBuses(strings.Split(scanner.Text(), ","))
+	fmt.Printf("buses: %v\n", buses)
+	fmt.Println(findConvergentTime(buses))
 }
