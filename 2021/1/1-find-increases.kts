9673df4137e1261cbc556aa1b7f010424d97530c
Sol Mumey <smumey@gmail.com>
2021-12-01 20:38:04 -0700

    aoc2021 1b

--- 2021/1/1-find-increases.kts
+++ 2021/1/1-find-increases.kts
@@ -3,10 +3,10 @@ main()
 fun main() {
     println(
         generateSequence(::readLine)
-        .map(Integer::parseInt)
+        .map(String::toInt)
         .fold(
-            intArrayOf(0, Integer.MAX_VALUE),
-            { acc: IntArray, i: Int -> if (i > acc[1]) intArrayOf(acc[0] + 1, i) else intArrayOf(acc[0], i) }
+            intArrayOf(0, Int.MAX_VALUE, Int.MAX_VALUE, Int.MAX_VALUE),
+            { acc, i: Int -> if (i > acc[1]) intArrayOf(acc[0] + 1, acc[2], acc[3], i) else intArrayOf(acc[0], acc[2], acc[3], i) }
         )[0]
     )
 }
