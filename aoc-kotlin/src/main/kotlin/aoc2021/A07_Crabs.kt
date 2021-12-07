0cc8926f9aacb36c2d76b66585d2d8ecb08b8129
Sol Mumey <smumey@gmail.com>
2021-12-07 00:08:33 -0700

    aoc2021 7b

--- aoc-kotlin/src/main/kotlin/aoc2021/A07_Crabs.kt
+++ aoc-kotlin/src/main/kotlin/aoc2021/A07_Crabs.kt
@@ -2,9 +2,19 @@ package aoc2021
 
 import kotlin.math.abs
 
+fun price(position: Int, target: Int): Int {
+	val d = abs(position - target)
+	return (d * (d + 1)) / 2
+}
 
 fun main() {
 	val positions = readLine()?.split(",")?.map(String::toInt) ?: listOf()
-	val mid = positions.sorted()[positions.size/2] // sum / positions.size + if ((sum % positions.size) * 2 > positions.size) 1 else 0
-	println(positions.fold(0) { distance, pos -> distance + abs(pos - mid) })
+	val min = positions.minOrNull() ?: 0
+	val max = positions.maxOrNull() ?: 0
+	println(
+		(min..max).fold(Pair(-1, Int.MAX_VALUE)) { current, target ->
+			val newCost = positions.sumOf { price(it, target) }
+			if (newCost < current.second) Pair(target, newCost) else current
+		}
+	)
 }
