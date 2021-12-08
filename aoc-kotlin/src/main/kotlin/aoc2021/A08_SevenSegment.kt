0b33f09c26c6e5284b58276b8fdf1cc27d151e91
Sol Mumey <smumey@gmail.com>
2021-12-08 00:50:09 -0700

    aoc2021 8b

--- aoc-kotlin/src/main/kotlin/aoc2021/A08_SevenSegment.kt
+++ aoc-kotlin/src/main/kotlin/aoc2021/A08_SevenSegment.kt
@@ -1,18 +1,60 @@
 package aoc2021
 
-data class Reading(val inputs: List<String>, val outputs: List<String>)
+data class Reading(val inputs: Set<String>, val outputs: List<String>)
 
 fun toReading(line: String): Reading {
 	val (inputS, outputS) = line.split(" | ")
-	return Reading(inputS.split(" "), outputS.split(" "))
+	return Reading(
+		inputS.split(" ").map { it.asSequence().sorted().joinToString("") }.toSet(),
+		outputS.split(" ").map { it.asSequence().sorted().joinToString("") }
+	)
+}
+
+fun deduce(inputs: Set<String>): Map<String, Int> {
+	val remaining = inputs.toMutableList()
+	val one = remaining.first { it.length == 2 }
+	remaining.remove(one)
+	val four = remaining.first { it.length == 4 }
+	remaining.remove(four)
+	val seven = remaining.first { it.length == 3 }
+	remaining.remove(seven)
+	val eight = inputs.first { it.length == 7 }
+	remaining.remove(eight)
+	val six = remaining.first { it.length == 6 && it.toSet().subtract(one.toSet()).size == 5 }
+	remaining.remove(six)
+	val nine = remaining.first { it.length == 6 && it.toSet().subtract(four.toSet()).size == 2 }
+	remaining.remove(nine)
+	val zero = remaining.first { it.length == 6 && it.toSet().subtract(four.toSet()).size == 3 }
+	remaining.remove(zero)
+	val three = remaining.first { it.length == 5 && it.toSet().subtract(one.toSet()).size == 3 }
+	remaining.remove(three)
+	val five = remaining.first { it.length == 5 && six.toSet().subtract(it.toSet()).size == 1 }
+	remaining.remove(five)
+	val two = remaining.first()
+	remaining.remove(two)
+	return mapOf(
+		zero to 0,
+		one to 1,
+		two to 2,
+		three to 3,
+		four to 4,
+		five to 5,
+		six to 6,
+		seven to 7,
+		eight to 8,
+		nine to 9
+	)
 }
 
 fun main() {
 	val readings = generateSequence(::readLine).map(::toReading).toList()
-	println(readings)
 	println(
 		readings
-			.flatMap { r -> r.outputs }
-			.count { w -> setOf(2, 4, 3, 7).contains(w.length) }
+			.map {
+				val cipher = deduce(it.inputs)
+				it.outputs.map { code -> cipher[code] }
+					.fold(0) { value, digit -> 10 * value + (digit ?: Int.MAX_VALUE) }
+			}
+			.sum()
 	)
 }
