9da2a76e66a16bc0550806a1dd3b1b065203de62
Sol Mumey <smumey@gmail.com>
2021-12-11 18:28:49 -0700

    aoc2021 11b - functional

--- aoc-kotlin/src/main/kotlin/aoc2021/A11_DumboOctopuses.kt
+++ aoc-kotlin/src/main/kotlin/aoc2021/A11_DumboOctopuses.kt
@@ -1,28 +1,44 @@
 package aoc2021
 
-private data class Octopus(val energy: Int)
+private data class OctopusGrid(val rows: List<List<Int>>, val step: Int, val flashed: Int) {
+	val gridHeight: Int = rows.size
+	val gridWidth = rows[0].size
 
-private data class OctopusGrid(val rows: List<IntArray>) {
-	private val gridHeight: Int = rows.size
-	private val gridWidth = rows[0].size
-	var flashCount = 0
-
-	fun trigger(cell: Pair<Int, Int>) {
-		val j = cell.first
-		val i = cell.second
-		rows[j][i] += 1
-		if (rows[j][i] == 10) {
-			flashCount++
-			neighbours(cell).forEach(this::trigger)
-		}
+	fun trigger(cell: Pair<Int, Int>): OctopusGrid {
+		val (j, i) = cell
+		val grid = this.updateGrid(Pair(j, i), rows[j][i] + 1)
+		return if (grid.rows[j][i] == 10) {
+			this.neighbours(cell).fold(grid) { g, neighbour ->
+				g.trigger(neighbour)
+			}
+		} else grid
 	}
 
-	fun advance() {
-		flashCount = 0
-		rows.forEachIndexed { j, r ->
-			r.forEachIndexed { i, _ -> trigger(Pair(j, i)) }
+	fun beginStep(): OctopusGrid {
+		return OctopusGrid(this.rows.map { r ->
+			r.map { c -> if (c > 9) 0 else c }
+		}, step + 1,0)
+	}
+
+	fun updateGrid(cell: Pair<Int, Int>, value: Int): OctopusGrid {
+		val (j, i) = cell
+		return OctopusGrid(
+			rows.subList(0, j)
+				.plusElement(rows[j].subList(0, i) + value + rows[j].subList(i + 1, rows[j].size))
+				.plus(rows.subList(j + 1, rows.size)),
+			this.step,
+			if (value == 10) flashed + 1 else flashed
+		)
+	}
+
+	fun advance(): OctopusGrid {
+		val grid = beginStep()
+		// println("${grid.step}: triggered ${grid.flashed}" )
+		return grid.rows.foldIndexed(grid) { j, g, row ->
+			row.foldIndexed(g) { i, h, _ ->
+				h.trigger(Pair(j, i))
+			}
 		}
-		rows.forEach { r -> r.forEachIndexed { i, _ -> if (r[i] > 9) r[i] = 0 } }
 	}
 
 	fun neighbours(cell: Pair<Int, Int>): List<Pair<Int, Int>> {
@@ -36,27 +52,18 @@ private data class OctopusGrid(val rows: List<IntArray>) {
 					.map { ni -> Pair(nj, ni) }
 			}.toList()
 	}
-
-	fun repeatUntilSync(): Int {
-		var step = 0
-		while (flashCount != gridWidth * gridHeight) {
-			advance()
-			step += 1
-		}
-		return step
-	}
-
-	override fun toString(): String {
-		return rows.joinToString("\n", transform = IntArray::contentToString)
-	}
-
 }
 
 fun main() {
 	val grid = OctopusGrid(
 		generateSequence(::readLine)
-			.map { it.toCharArray().map { c -> c - '0' }.toIntArray() }
-			.toList()
+			.map { it.toCharArray().map { c -> c - '0' }.toList() }
+			.toList(),
+		0,
+		0
 	)
-	println(grid.repeatUntilSync())
+	val final =
+		generateSequence(grid, OctopusGrid::advance)
+			.first { g -> g.flashed == g.gridHeight * g.gridWidth }
+	println(final.step)
 }
