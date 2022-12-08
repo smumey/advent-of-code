0aed2030317784e037b8fc9363af51722e465c05
Sol Mumey <smumey@gmail.com>
2022-12-08 00:44:25 -0700

    aoc2022 8

---
+++ aoc-kotlin/src/main/kotlin/aoc2022/d08/D08_TreeFort.kt
@@ -0,0 +1,75 @@
+package aoc2022.d08
+
+import readInput
+
+enum class Direction {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+}
+
+data class Coordinate(val x: Int, val y: Int)
+
+data class TreePatch(val trees: List<Int>, val width: Int) {
+	fun isInside(coord: Coordinate): Boolean {
+		return coord.x in 0 until width && coord.y * width in 0 until trees.size
+	}
+
+	fun move(coord: Coordinate, direction: Direction): Coordinate? {
+		val new = when (direction) {
+			Direction.UP -> Coordinate(coord.x, coord.y - 1)
+			Direction.RIGHT -> Coordinate(coord.x + 1, coord.y)
+			Direction.DOWN -> Coordinate(coord.x, coord.y + 1)
+			Direction.LEFT -> Coordinate(coord.x - 1, coord.y)
+		}
+		return if (isInside(new)) new else null
+	}
+
+	fun height(coord: Coordinate): Int {
+		return trees[coord.y * width + coord.x]
+	}
+
+	fun interveningTrees(coord: Coordinate, direction: Direction): Sequence<Coordinate> {
+		return generateSequence(coord) { coord -> move(coord, direction) }.drop(1)
+	}
+
+	fun countVisible(): Int {
+		return trees.indices.map { coordinate(it) }.count {
+			Direction.values().any { direction ->
+				interveningTrees(it, direction).map { t -> height(t) }.all { h -> h < height(it) }
+			}
+		}
+	}
+
+	private fun coordinate(index: Int) = Coordinate(index % width, index / width)
+
+	fun scenicScore(coord: Coordinate): Int {
+		return Direction.values()
+			.map { direction ->
+				val list = interveningTrees(coord, direction).toList()
+				var count = 0
+				for (tree in list) {
+					count += 1
+					if (height(tree) >= height(coord)) {
+						break
+					}
+				}
+				count
+			}
+			.reduce { s1, s2 -> s1 * s2 }
+	}
+
+	fun maxScenicScore(): Int {
+		return trees.indices.map { it -> coordinate(it) }.maxOf { scenicScore(it) }
+	}
+}
+
+fun parse(input: List<String>): TreePatch {
+	val rows = input.map { it.trim().windowed(1).map(String::toInt) }
+	return TreePatch(rows.flatten(), rows[0].size)
+}
+
+fun main() {
+	println(parse(readInput("aoc2022/8")).maxScenicScore())
+}
---
+++ aoc-kotlin/src/test/kotlin/aoc2022/d08/D08_TreeFortTest.kt
@@ -0,0 +1,34 @@
+package aoc2022.d08
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import readTestInput
+
+class D08_TreeFortTest {
+	val trees = parse(readTestInput("aoc2022/8"))
+
+	@Test
+	fun interveningTrees_left() {
+		assertEquals(listOf(Coordinate(0, 0)), trees.interveningTrees(Coordinate(1, 0), Direction.LEFT).toList())
+	}
+
+	@Test
+	fun interveningTrees_up() {
+		assertEquals(listOf(Coordinate(0, 0)), trees.interveningTrees(Coordinate(0, 1), Direction.UP).toList())
+	}
+
+	@Test
+	fun countVisible() {
+		assertEquals(21, trees.countVisible())
+	}
+
+	@Test
+	fun scenicScore() {
+		assertEquals(4, trees.scenicScore(Coordinate(2, 1)))
+	}
+
+	@Test
+	fun maxScenicScore() {
+		assertEquals(8, trees.maxScenicScore())
+	}
+}
