84134e5e9e1793553ad7765d9ef7f7b3ccb61f6a
Sol Mumey <smumey@gmail.com>
2022-12-11 04:10:27 -0700

    aoc2022 11.2

--- aoc-kotlin/src/main/kotlin/aoc2022/d11/D11_MonkeyingAround.kt
+++ aoc-kotlin/src/main/kotlin/aoc2022/d11/D11_MonkeyingAround.kt
@@ -1,23 +1,28 @@
 package aoc2022.d11
 
 import aoc.readInput
-import java.util.function.IntUnaryOperator
+import java.util.function.LongUnaryOperator
 
-typealias ItemsList = List<List<Int>>
+typealias ItemsList = List<List<Long>>
 
-data class Monkey(val index: Int, val operation: IntUnaryOperator, val test: IntUnaryOperator) {
-	fun processItems(items: ItemsList): ItemsList {
+data class Monkey(
+	val index: Int,
+	val operation: LongUnaryOperator,
+	val divisor: Long,
+	val trueTarget: Int,
+	val falseTarget: Int
+) {
+	fun processItems(items: ItemsList, boredom: Long, modulo: Long): ItemsList {
 		val ownItems = items[index]
-		val newItems = ownItems.map { it -> growBored(inspect(it)) }
+		val newItems = ownItems.map { (inspect(it) / boredom) % modulo }
 		return items.mapIndexed { i, monkeyItems ->
-			(if (i == index) listOf<Int>() else monkeyItems) + newItems.filter { determineTarget(it) == i }
+			(if (i == index) listOf() else monkeyItems) + newItems.filter { determineTarget(it) == i }
 		}
 	}
 
-	fun determineTarget(item: Int) = test.applyAsInt(item)
+	fun determineTarget(item: Long) = if (item % divisor == 0L) trueTarget else falseTarget
 
-	fun inspect(item: Int) = operation.applyAsInt(item)
-	fun growBored(item: Int) = item / 3
+	fun inspect(item: Long) = operation.applyAsLong(item)
 }
 
 data class State(val monkeys: List<Monkey>, val roundState: RoundState)
@@ -31,10 +36,10 @@ fun parse(input: List<String>): State {
 	return State(pairs.map { it.first }, RoundState(pairs.map { it.second }, pairs.map { 0 }))
 }
 
-fun parseMonkey(lines: Iterator<String>): Pair<Monkey, List<Int>>? {
+fun parseMonkey(lines: Iterator<String>): Pair<Monkey, List<Long>>? {
 	var index = -1
 	while (lines.hasNext()) {
-		var line = lines.next()
+		val line = lines.next()
 		if (line.isNotEmpty()) {
 			index = line.substring("Monkey ".length, line.length - 1).toInt()
 			break
@@ -42,9 +47,9 @@ fun parseMonkey(lines: Iterator<String>): Pair<Monkey, List<Int>>? {
 	}
 	if (!lines.hasNext()) return null
 	val itemLine = lines.next()
-	val items = itemLine.substring("  Starting items: ".length).split(", ").map(String::toInt)
+	val items = itemLine.substring("  Starting items: ".length).split(", ").map(String::toLong)
 	val (xS, operatorString, yS) = lines.next().substring("  Operation: new = ".length).split(" ")
-	val divisor = lines.next().substring("  Test: divisible by ".length).toInt()
+	val divisor = lines.next().substring("  Test: divisible by ".length).toLong()
 	val trueMonkey = lines.next().substring("    If true: throw to monkey ".length).toInt()
 	val falseMonkey = lines.next().substring("    If false: throw to monkey ".length).toInt()
 	val operator = parseOperator(operatorString)
@@ -55,13 +60,15 @@ fun parseMonkey(lines: Iterator<String>): Pair<Monkey, List<Int>>? {
 		Monkey(
 			index,
 			{ item -> operator(x(item), y(item)) },
-			{ item -> if (item % divisor == 0) trueMonkey else falseMonkey }
+			divisor,
+			trueMonkey,
+			falseMonkey
 		),
 		items
 	)
 }
 
-private fun parseOperator(operatorString: String): (Int, Int) -> Int {
+private fun parseOperator(operatorString: String): (Long, Long) -> Long {
 	return when (operatorString) {
 		"+" -> ({ i, j -> i + j })
 		"*" -> ({ i, j -> i * j })
@@ -69,18 +76,18 @@ private fun parseOperator(operatorString: String): (Int, Int) -> Int {
 	}
 }
 
-private fun parseOperand(operandString: String): (Int) -> Int = when (operandString) {
+private fun parseOperand(operandString: String): (Long) -> Long = when (operandString) {
 	"old" -> ({ it })
 	else -> {
-		val value = operandString.toInt()
+		val value = operandString.toLong()
 		({ value })
 	}
 }
 
-fun executeRound(roundStateAtStart: RoundState, monkeys: List<Monkey>): RoundState {
+fun executeRound(roundStateAtStart: RoundState, monkeys: List<Monkey>, divisor: Long, modulo: Long): RoundState {
 	return monkeys.fold(roundStateAtStart) { roundState, monkey ->
 		RoundState(
-			monkey.processItems(roundState.items),
+			monkey.processItems(roundState.items, divisor, modulo),
 			roundState.inspectCounts.mapIndexed { i, count ->
 				when (i) {
 					monkey.index -> count + roundState.items[i].size
@@ -91,18 +98,37 @@ fun executeRound(roundStateAtStart: RoundState, monkeys: List<Monkey>): RoundSta
 	}
 }
 
-fun executeRounds(roundStateAtStart: RoundState, monkeys: List<Monkey>, rounds: Int): RoundState {
-	return generateSequence(roundStateAtStart) { roundState -> executeRound(roundState, monkeys) }
+fun executeRounds(
+	roundStateAtStart: RoundState,
+	monkeys: List<Monkey>,
+	divisor: Long,
+	modulo: Long,
+	rounds: Int,
+): RoundState {
+	return generateSequence(roundStateAtStart) { roundState -> executeRound(roundState, monkeys, divisor, modulo) }
 		.take(rounds + 1)
 		.last()
 }
 
-fun findActiveMonkeyScore(inspectCounts: List<Int>): Int {
-	return inspectCounts.sortedDescending().take(2).reduce { i, j -> i * j }
+fun findActiveMonkeyScore(inspectCounts: List<Int>): Long {
+	return inspectCounts.sortedDescending().take(2).fold(1L) { i, j -> i * j }
+}
+
+fun getDivisorProduct(monkeys: List<Monkey>): Long {
+	return monkeys.map { it.divisor }.reduce { d1, d2 -> d1 * d2 }
 }
 
 fun main() {
 	val state = parse(readInput("aoc2022/11"))
-	println(findActiveMonkeyScore(executeRounds(state.roundState, state.monkeys, 20).inspectCounts))
-
+	println(
+		findActiveMonkeyScore(
+			executeRounds(
+				state.roundState,
+				state.monkeys,
+				1,
+				getDivisorProduct(state.monkeys),
+				10000
+			).inspectCounts
+		)
+	)
 }
--- aoc-kotlin/src/test/kotlin/aoc2022/d11/D11_MonkeyingAroundTest.kt
+++ aoc-kotlin/src/test/kotlin/aoc2022/d11/D11_MonkeyingAroundTest.kt
@@ -14,14 +14,43 @@ class D11_MonkeyingAroundTest {
 
 	@Test
 	fun processRound() {
-		println(executeRound(state.roundState, state.monkeys))
+		println(executeRound(state.roundState, state.monkeys, 3, getDivisorProduct(state.monkeys)))
 	}
 
 	@Test
 	fun findActiveMonkeyScore() {
 		assertEquals(
 			10605,
-			findActiveMonkeyScore(executeRounds(state.roundState, state.monkeys, 20).inspectCounts)
+			findActiveMonkeyScore(
+				executeRounds(
+					state.roundState,
+					state.monkeys,
+					3,
+					getDivisorProduct(state.monkeys),
+					20
+				).inspectCounts
+			)
+		)
+	}
+
+	@Test
+	fun executeRoundsNoBoredom() {
+		println(executeRounds(state.roundState, state.monkeys, 1, getDivisorProduct(state.monkeys), 10))
+	}
+
+	@Test
+	fun findActiveMonkeyScore2() {
+		assertEquals(
+			2713310158L,
+			findActiveMonkeyScore(
+				executeRounds(
+					state.roundState,
+					state.monkeys,
+					1,
+					getDivisorProduct(state.monkeys),
+					10000
+				).inspectCounts
+			)
 		)
 	}
 }
