24c762e027ec1d332cd6280c17555face1d58fa4
Sol Mumey <smumey@gmail.com>
2022-12-15 02:23:54 -0700

    aoc2022 15.2 extremely slow

--- aoc-kotlin/src/main/kotlin/aoc2022/d15/D15_BeaconExclusionZone.kt
+++ aoc-kotlin/src/main/kotlin/aoc2022/d15/D15_BeaconExclusionZone.kt
@@ -1,51 +1,86 @@
 package aoc2022.d15
 
-import aoc.LongCoordinate
+import aoc.Coordinate
 import aoc.readInput
+import java.util.BitSet
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
-data class Sensor(val location: LongCoordinate, val closestBeacon: LongCoordinate) {
-	fun distance(coordinate: LongCoordinate) = abs(coordinate.x - location.x) + abs(coordinate.y - location.y)
+data class Sensor(val location: Coordinate, val closestBeacon: Coordinate) {
+	fun distance(coordinate: Coordinate) = abs(coordinate.x - location.x) + abs(coordinate.y - location.y)
 
-	fun findCoveredPositions(y: Long): List<LongCoordinate> {
+	fun findCoveredPositions(y: Int): IntRange {
 		val beaconDistance = distance(closestBeacon)
 		val yDist = abs(y - location.y)
-		if (yDist > beaconDistance) return listOf()
-		return ((location.x - (beaconDistance - yDist))..(location.x + (beaconDistance - yDist))).map {
-			LongCoordinate(it, y)
-		}
+		if (yDist > beaconDistance) return IntRange.EMPTY
+		return ((location.x - (beaconDistance - yDist))..(location.x + (beaconDistance - yDist)))
 	}
 }
 
 fun parse(line: String): Sensor {
-	val locX: Long
-	val locY: Long
-	val beaconX: Long
-	val beaconY: Long
+	val locX: Int
+	val locY: Int
+	val beaconX: Int
+	val beaconY: Int
 	line.substring("Sensor at x=".length).let { locXSub ->
 		val locXEnd = locXSub.indexOf(',')
-		locX = locXSub.substring(0, locXEnd).toLong()
+		locX = locXSub.substring(0, locXEnd).toInt()
 		locXSub.substring(locXEnd + ", y=".length).let { locYSub ->
 			val locYEnd = locYSub.indexOf(':')
-			locY = locYSub.substring(0, locYEnd).toLong()
+			locY = locYSub.substring(0, locYEnd).toInt()
 			locYSub.substring(locYEnd + ": closest beacon is at x=".length).let { beaconXSub ->
 				val beaconXEnd = beaconXSub.indexOf(',')
-				beaconX = beaconXSub.substring(0, beaconXEnd).toLong()
+				beaconX = beaconXSub.substring(0, beaconXEnd).toInt()
 				beaconXSub.substring(beaconXEnd + ", y=".length).let { beaconYSub ->
-					beaconY = beaconYSub.toLong()
+					beaconY = beaconYSub.toInt()
 				}
 			}
 		}
 	}
-	return Sensor(LongCoordinate(locX, locY), LongCoordinate(beaconX, beaconY))
+	return Sensor(Coordinate(locX, locY), Coordinate(beaconX, beaconY))
 
 }
 
-fun findBeaconExclusionCount(sensors: List<Sensor>, y: Long): Int {
-	return sensors.flatMap { it.findCoveredPositions(y) }.toSet().minus(sensors.map { it.closestBeacon }.toSet()).size
+data class BitSetWithOffset(val bitSet: BitSet, val minX: Int)
+
+fun findBeaconExclusions(sensors: List<Sensor>, y: Int): BitSetWithOffset {
+	val bitSet = BitSet()
+	val ranges = sensors.map { it.findCoveredPositions(y) }
+	val minX = min(ranges.minOf { it.first }, sensors.minOf { it.closestBeacon.x })
+	ranges.forEach {
+		bitSet.set(it.first - minX, it.last + 1 - minX)
+	}
+	sensors.filter { it.closestBeacon.y == y }.map { it.closestBeacon.x }.forEach {
+		bitSet.clear(it - minX)
+	}
+	return BitSetWithOffset(bitSet, minX)
+}
+
+fun tuningFrequency(coordinate: Coordinate): Long {
+	return coordinate.x * 4000000L + coordinate.y
+}
+
+fun findBeaconLocation(sensors: List<Sensor>, maxX: Int, maxY: Int): Coordinate? {
+	val bitSet = BitSet()
+	val pair = (0..maxY).asSequence()
+		.map { y ->
+			bitSet.clear()
+			val ranges = sensors.map { it.findCoveredPositions(y) }
+			ranges.forEach {
+				bitSet.set(max(it.first, 0), min(it.last, maxX) + 1)
+			}
+			Pair(y, bitSet)
+
+		}
+		.find { it.second.nextClearBit(0) <= maxX }
+	return pair?.let {
+		val x = it.second.nextClearBit(0)
+		Coordinate(x, it.first)
+	}
 }
 
 fun main() {
 	val sensors = readInput("aoc2022/15").map(::parse)
-	println(findBeaconExclusionCount(sensors, 2000000))
+	println(findBeaconExclusions(sensors, 2000000).bitSet.cardinality())
 }
--- aoc-kotlin/src/test/kotlin/aoc2022/d15/D15_BeaconExclusionZoneTest.kt
+++ aoc-kotlin/src/test/kotlin/aoc2022/d15/D15_BeaconExclusionZoneTest.kt
@@ -1,5 +1,6 @@
 package aoc2022.d15
 
+import aoc.Coordinate
 import aoc.readInput
 import aoc.readTestInput
 import kotlin.test.assertEquals
@@ -16,16 +17,35 @@ class D15_BeaconExclusionZoneTest {
 
 	@Test
 	fun findCoveredPositions() {
-		assertEquals(13, testSensors[6].findCoveredPositions(10).size)
+		assertEquals(13, testSensors[6].findCoveredPositions(10).count())
 	}
 
 	@Test
 	fun findBeaconExclusionTestCount() {
-		assertEquals(26, findBeaconExclusionCount(testSensors, 10))
+		assertEquals(26, findBeaconExclusions(testSensors, 10).bitSet.cardinality())
 	}
 
 	@Test
 	fun findBeaconExclusionCount() {
-		assertEquals(0, findBeaconExclusionCount(sensors, 2000000))
+		assertEquals(4502208, findBeaconExclusions(sensors, 2000000).bitSet.cardinality())
+	}
+
+	@Test
+	fun findTestBeaconLocation() {
+		assertEquals(Coordinate(14, 11), findBeaconLocation(testSensors, 20, 20))
+	}
+
+	@Test
+	fun findTestFreq() {
+		findBeaconLocation(testSensors, 20, 20)?.let {
+			assertEquals(56000011, tuningFrequency(it))
+		}
+	}
+
+	@Test
+	fun findFreq() {
+		findBeaconLocation(sensors, 4000000, 4000000)?.let {
+			assertEquals(0, tuningFrequency(it))
+		}
 	}
 }
