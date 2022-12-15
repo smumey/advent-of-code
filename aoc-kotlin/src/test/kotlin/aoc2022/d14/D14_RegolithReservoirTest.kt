f63947ab4a9cabbda6da5412e91e6701ce01e260
Sol Mumey <smumey@gmail.com>
2022-12-14 21:58:09 -0700

    aoc2022 14 add test for actual input

--- aoc-kotlin/src/test/kotlin/aoc2022/d14/D14_RegolithReservoirTest.kt
+++ aoc-kotlin/src/test/kotlin/aoc2022/d14/D14_RegolithReservoirTest.kt
@@ -1,21 +1,29 @@
 package aoc2022.d14
 
+import aoc.readInput
 import aoc.readTestInput
 import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
 
 class D14_RegolithReservoirTest {
-	val rockCoordinates = readTestInput("aoc2022/14").map(::parse).flatMap(::filled).toSet()
+	val rockTestCoordinates = readTestInput("aoc2022/14").map(::parse).flatMap(::filled).toSet()
+	val rockCoordinates = readInput("aoc2022/14").map(::parse).flatMap(::filled).toSet()
 
 	@Test
 	fun sandRestVoidCount() {
-		val initialState = SandState(maxDepth(rockCoordinates), rockCoordinates, sandSource, 0)
+		val initialState = SandState(maxDepth(rockTestCoordinates), rockTestCoordinates, sandSource, 0)
 		assertEquals(24, generateSequence(initialState) { it.stepVoid() }.last().sandRestCount)
+	}
+
+	@Test
+	fun sandRestInfiniteFloorCountTest() {
+		val initialState = SandState(maxDepth(rockTestCoordinates), rockTestCoordinates, sandSource, 0)
+		assertEquals(93, generateSequence(initialState) { it.stepInfiniteFloor() }.last().sandRestCount)
 	}
 
 	@Test
 	fun sandRestInfiniteFloorCount() {
 		val initialState = SandState(maxDepth(rockCoordinates), rockCoordinates, sandSource, 0)
-		assertEquals(93, generateSequence(initialState) { it.stepInfiniteFloor() }.last().sandRestCount)
+		assertEquals(22499, generateSequence(initialState) { it.stepInfiniteFloor() }.last().sandRestCount)
 	}
 }
