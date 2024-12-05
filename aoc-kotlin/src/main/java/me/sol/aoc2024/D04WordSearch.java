9b92c8c11d71b77acdb4fbc123da4de9e3787478
Sol Mumey <smumey@gmail.com>
2024-12-05 00:42:03 -0700

    aoc2024 4.1

--- aoc-kotlin/src/main/java/me/sol/aoc2024/D04WordSearch.java
+++ aoc-kotlin/src/main/java/me/sol/aoc2024/D04WordSearch.java
@@ -1,56 +1,105 @@
 package me.sol.aoc2024;
 
+import aoc.Coordinate;
+import me.sol.Utility;
+
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class D04WordSearch {
     private static final String search = "XMAS";
     private static final String reversedSearch = "SAMX";
+    private final Puzzle puzzle;
+
+    D04WordSearch(Puzzle puzzle) {
+        this.puzzle = puzzle;
+    }
 
     static Puzzle parse(Stream<String> lines) {
         return new Puzzle(lines.toArray(String[]::new));
     }
 
-    record Puzzle(String[] lines) {
-        int size() {
-            return lines.length;
-        }
-
-        Stream<String> horizontals() {
-            return Arrays.stream(lines);
-        }
-
-        Stream<String> verticals() {
-            return IntStream.range(0, size())
-                    .mapToObj(
-                            j -> IntStream.range(0, size())
-                                    .mapToObj(i -> lines[i].charAt(j))
-                                    .collect(StringBuilder::new, StringBuilder::append, (b1, b2) -> b1.append(b2))
-                                    .toString()
-
-                    );
-        }
-
-        Stream<String> diagonals(boolean up) {
-            var n = size();
-            return IntStream.range(0, n)
-                    .mapToObj(
-                            c -> {
-                                var l = n - Math.abs();
-                                return IntStream.range(0, l)
-                                        .mapToObj(k -> {
-                                            var xPos = up ? n - 1 - c - (l / 2) + k : c - (l / 2) + k;
-                                            var yPos = up ? c - (l / 2) + k : n - 1 - c - (l / 2) + k;
-                                            System.out.printf("up=%b, c=%d, l=%d, xPos=%d, yPos=%d%n", up, c, l, xPos, yPos);
-                                            return lines[yPos].charAt(xPos);
-                                        })
-                                        .collect(StringBuilder::new, StringBuilder::append, (b1, b2) -> b1.append(b2))
-                                        .toString();
-                            }
-                    );
-        }
+    public static void main(String[] args) throws IOException {
+        System.out.println(new D04WordSearch(Utility.readInput(D04WordSearch.class, D04WordSearch::parse)).countWords());
     }
 
+    int countMatches(Pattern pattern) {
+        return Stream.of(puzzle.horizontals(), puzzle.verticals(), puzzle.diagonals(false), puzzle.diagonals(true))
+                .flatMap(Function.identity())
+                .mapToInt(line -> {
+                    var m = pattern.matcher(line);
+                    var count = 0;
+                    while (m.find()) {
+                        count += 1;
+                    }
+                    System.out.printf("pattern=%s, line=%s count=%d%n", pattern, line, count);
+                    return count;
+                })
+                .sum();
+    }
 
+    int countWords() {
+        var forward = Pattern.compile(search);
+        var reverse = Pattern.compile(reversedSearch);
+        return countMatches(Pattern.compile(search)) + countMatches(Pattern.compile(reversedSearch));
+    }
 }
+
+record Puzzle(String[] lines) {
+    int size() {
+        return lines.length;
+    }
+
+    Stream<String> horizontals() {
+        return Arrays.stream(lines);
+    }
+
+    Stream<String> verticals() {
+        return IntStream.range(0, size())
+                .mapToObj(
+                        j -> IntStream.range(0, size())
+                                .mapToObj(i -> lines[i].charAt(j))
+                                .collect(StringBuilder::new, StringBuilder::append, (b1, b2) -> b1.append(b2))
+                                .toString()
+
+                );
+    }
+
+    private boolean inbounds(Coordinate coordinate) {
+        int n = size();
+        int x = coordinate.getX();
+        int y = coordinate.getY();
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+
+    Stream<String> diagonals(boolean up) {
+        var n = size();
+        return IntStream.range(0, 2 * n - 1)
+                .mapToObj(
+                        yOffset -> {
+                            return IntStream.range(0, n)
+                                    .mapToObj(x -> {
+                                        var y = up ? -n + 1 + yOffset + x : 2 * n - 2 - yOffset - x;
+                                        var coordinate = new Coordinate(x, y);
+//                                        System.out.printf(
+//                                                "up=%b, yOffset=%d, x=%d, coordinate=%s%n",
+//                                                up,
+//                                                yOffset,
+//                                                x,
+//                                                coordinate
+//                                        );
+                                        return coordinate;
+                                    })
+                                    .filter(this::inbounds)
+                                    .map(c -> lines[c.getY()].charAt(c.getX()))
+                                    .collect(StringBuilder::new, StringBuilder::append, (b1, b2) -> b1.append(b2))
+                                    .toString();
+                        }
+                );
+    }
+}
+
--- aoc-kotlin/src/test/java/me/sol/aoc2024/D04WordSearchTest.java
+++ aoc-kotlin/src/test/java/me/sol/aoc2024/D04WordSearchTest.java
@@ -4,13 +4,13 @@ import me.sol.Utility;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class D04WordSearchTest {
     @Test
     void count() throws IOException {
         var puzzle = Utility.readSample(this.getClass(), D04WordSearch::parse);
-        System.out.println(Arrays.toString(puzzle.lines()));
-        System.out.println(puzzle.diagonals(false).toList());
+        assertThat(new D04WordSearch(puzzle).countWords()).isEqualTo(18);
     }
 }
