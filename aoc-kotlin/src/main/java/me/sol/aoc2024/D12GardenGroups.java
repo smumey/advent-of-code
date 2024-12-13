46980d09deb147d6f829d3564ab5d7b41333758b
Sol Mumey <smumey@gmail.com>
2024-12-12 23:07:19 -0700

    aoc2024 12.2

--- aoc-kotlin/src/main/java/me/sol/aoc2024/D12GardenGroups.java
+++ aoc-kotlin/src/main/java/me/sol/aoc2024/D12GardenGroups.java
@@ -1,12 +1,15 @@
 package me.sol.aoc2024;
 
+import aoc.Direction;
 import me.sol.Answer;
 import me.sol.Grid;
 import me.sol.Utility;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.stream.Stream;
 
 public class D12GardenGroups {
     private final Grid grid;
@@ -17,6 +20,26 @@ public class D12GardenGroups {
 
     public static void main(String[] args) throws IOException {
         Utility.execute(new D12GardenGroups(Utility.readInput(D12GardenGroups.class, Utility::parseCharGrid)));
+    }
+
+    private static void clear(int[][] array) {
+        Arrays.stream(array).forEach(a -> Arrays.fill(a, -1));
+    }
+
+    private int countSides(int[][] lineSegments) {
+        int sides = 0;
+        for (var segments : lineSegments) {
+            int previousLine = -1;
+            for (var segment : segments) {
+                if (segment != -1) {
+                    if (segment != previousLine) {
+                        sides += 1;
+                    }
+                }
+                previousLine = segment;
+            }
+        }
+        return sides;
     }
 
     @Answer
@@ -62,16 +85,102 @@ public class D12GardenGroups {
                 assigned.add(regionPlot);
                 area += 1;
                 regionPerimeter += perimeter;
-                System.out.printf("Adding perimeter %d for %d,%d to %c%n", perimeter, grid.getX(regionPlot), grid.getY(regionPlot), (char) plantType);
+//                System.out.printf("Adding perimeter %d for %d,%d to %c%n", perimeter, grid.getX(regionPlot), grid.getY(regionPlot), (char) plantType);
             }
 
             cost += regionPerimeter * area;
+//            System.out.printf(
+//                    "Adding region of type %c, regionPerimeter %d, area %d, cost %d%n",
+//                    (char) plantType,
+//                    regionPerimeter,
+//                    area,
+//                    regionPerimeter * area
+//            );
+        }
+        return cost;
+    }
+
+    private int[][] makeLineSegmentArray(int positions, int length) {
+        return Stream.generate(() -> new int[length])
+                .limit(positions)
+                .toArray(int[][]::new);
+    }
+
+    @Answer
+    int p2Cost() {
+        var explored = new HashSet<Integer>();
+        var cost = 0;
+        var coord = 0;
+        var queue = new LinkedList<Integer>();
+        var verticalLineSegments = makeLineSegmentArray(grid.width() + 1, grid.height());
+        var horizontalLineSegments = makeLineSegmentArray(grid.height() + 1, grid.width());
+        queue.add(coord);
+        explored.add(coord);
+        var assigned = new HashSet<Integer>();
+        while (!queue.isEmpty()) {
+            var area = 0;
+            var plot = queue.removeFirst();
+            if (assigned.contains(plot)) {
+                continue;
+            }
+            var plantType = grid.getValue(plot);
+            var regionQueue = new LinkedList<Integer>();
+            regionQueue.add(plot);
+            var regionExplored = new HashSet<Integer>();
+            regionExplored.add(plot);
+            clear(verticalLineSegments);
+            clear(horizontalLineSegments);
+            while (!regionQueue.isEmpty()) {
+                var regionPlot = regionQueue.removeFirst();
+                int x = grid.getX(regionPlot);
+                int y = grid.getY(regionPlot);
+                for (var d : Direction.values()) {
+                    int borderPlot = grid.move(regionPlot, d);
+                    if (borderPlot == Grid.OUT || grid.getValue(borderPlot) != plantType) {
+                        switch (d) {
+                            case UP:
+                                horizontalLineSegments[y][x] = d.ordinal();
+                                break;
+                            case RIGHT:
+                                verticalLineSegments[x + 1][y] = d.ordinal();
+                                break;
+                            case DOWN:
+                                horizontalLineSegments[y + 1][x] = d.ordinal();
+                                break;
+                            case LEFT:
+                                verticalLineSegments[x][y] = d.ordinal();
+                                break;
+                        }
+                        if (borderPlot != Grid.OUT) {
+                            if (!explored.contains(borderPlot)) {
+                                queue.add(borderPlot);
+                            }
+                        }
+                    } else {
+                        long neighbourType = grid.getValue(borderPlot);
+                        if (neighbourType == plantType) {
+                            if (!regionExplored.contains(borderPlot)) {
+                                regionQueue.add(borderPlot);
+                                regionExplored.add(borderPlot);
+                            }
+                        }
+                        explored.add(borderPlot);
+                    }
+                }
+
+                assigned.add(regionPlot);
+                area += 1;
+                System.out.printf("Adding %c region at %s%n", (char) plantType, grid.coordToString(regionPlot));
+            }
+
+            var regionSides = countSides(verticalLineSegments) + countSides(horizontalLineSegments);
+            cost += regionSides * area;
             System.out.printf(
-                    "Adding region of type %c, regionPerimeter %d, area %d, cost %d%n",
+                    "Adding region of type %c, sides %d, area %d, cost %d%n",
                     (char) plantType,
-                    regionPerimeter,
+                    regionSides,
                     area,
-                    regionPerimeter * area
+                    regionSides * area
             );
         }
         return cost;
--- aoc-kotlin/src/test/java/me/sol/aoc2024/D12GardenGroupsTest.java
+++ aoc-kotlin/src/test/java/me/sol/aoc2024/D12GardenGroupsTest.java
@@ -1,16 +1,62 @@
 package me.sol.aoc2024;
 
 import me.sol.Utility;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class D12GardenGroupsTest {
+    private static Stream<Arguments> p2TestMaps() {
+        return Stream.of(
+                Arguments.of(
+                        """
+                                AAAAAA
+                                AAABBA
+                                AAABBA
+                                ABBAAA
+                                ABBAAA
+                                AAAAAA
+                                """,
+                        368
+                ),
+                Arguments.of(
+                        """
+                                EEEEE
+                                EXXXX
+                                EEEEE
+                                EXXXX
+                                EEEEE
+                                """,
+                        236
+                )
+        );
+    }
 
     @Test
+    @Disabled
     void p1Cost() throws IOException {
         assertThat(new D12GardenGroups(Utility.readSample(getClass(), Utility::parseCharGrid)).p1Cost()).isEqualTo(1930);
     }
+
+    @Test
+    @Disabled
+    void p2Cost() throws IOException {
+        assertThat(new D12GardenGroups(Utility.readSample(getClass(), Utility::parseCharGrid)).p2Cost()).isEqualTo(1206);
+    }
+
+    @MethodSource("p2TestMaps")
+    @ParameterizedTest
+    void p2Cost(String map, int cost) {
+        var grid = Utility.parseCharGrid(Arrays.stream(map.split("\\n")));
+        assertThat(new D12GardenGroups(grid).p2Cost()).isEqualTo(cost);
+    }
+
 }
